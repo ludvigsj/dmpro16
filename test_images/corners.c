1ab3b87e95f72c1e5cf04d5b582506efe5e8c048
Katrine Roland <katrine.roland@gmail.com>
2016-09-23 20:43:36 +0200

    Add code for finding corners
    This code finds the two uppermost corners of the sudoku. It is tested on the
    8bit grayscale images. Currently the second corner will be one pixel of on
    images with no rotation.

--- test_images/corners.c
+++ test_images/corners.c
@@ -2,18 +2,57 @@
 
 int main()
 {
+	int first = 0;
+	int lastline = 0;
+	int curline = 0;
+	int fpx = -1;
+	int lpx = -1;
+	int lfpx = -1;
+
 	char buf[640];
-	FILE *fp = fopen("sudoku_bl_corner.raw", "rb");
+	FILE *fp = fopen("sudoku2_right_40deg8bit.gray", "r");
+
 	int i;
 	for (i = 0; i < 480; i++)
 	{
-		size_t returnval = fread(buf, 1, 640, fp);
+		fread(buf, 1, 640, fp);
 		int j;
-		printf("\n");
-		for (j = 0; j < 20); j++)
+		for (j = 0; j < 640; j++)
 		{
-			char temp = buf[j];
-			printf("%x", temp);
+			if (first == 0)
+			{
+				if (buf[j] != 0)
+				{
+					printf("First corner: %d, %d\n", i, j);
+					first = 1;
+					break;
+				}
+			}
+			else
+			{
+				if (buf[j] != 0)
+				{
+					if (fpx == -1)
+						fpx = j;
+					else
+						lpx = j;
+				}
+			}
+		}
+		if (first == 1)
+		{
+			lastline = curline;
+			curline = lpx - fpx;
+			if (curline == lastline && lastline > 0)
+			{
+				if (lfpx < fpx)
+					printf("Second corner: %d, %d\n", i-1, lfpx);
+				else
+					printf("Second corner: %d, %d\n", i-1, lpx);
+				break;
+			}
+			lfpx = fpx;
+			fpx = lpx = -1;
 		}
 	}
 
