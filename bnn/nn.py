d94e9e9e23112c26d6f877fcfd3fb44467a2bfcf
Olav Markussen <olavmarkussen@hotmail.no>
2016-10-01 18:54:23 +0200

    Rewrite how output layer is computed

--- bnn/nn.py
+++ bnn/nn.py
@@ -67,24 +67,18 @@ for test_image in range(total_images):
 
     # compute output layer (10)
     out_size = 10
-    input_out_layer = np.zeros(out_size)
-    out_layer_output = np.empty(out_size)
-    for neuron in range(out_size):
-        current_input = 0
-        for synapse_used in range(l2_size):
-            current_input += second_layer_output[synapse_used] * nn_out_synapses[synapse_used][neuron]
+    input_out_layer = np.dot(second_layer_output, nn_out_synapses)
     
-    # apply batchnorm
-        beta = nn_data["arr_14"][neuron]
-        gamma = nn_data["arr_15"][neuron]
-        mean = nn_data["arr_16"][neuron]
-        inv_stddev = nn_data["arr_17"][neuron]
-    
-        batchnorm = lambda x: gamma * (x - mean) * inv_stddev + beta
-        out_layer_output[neuron] = batchnorm( current_input )
+    beta = nn_data["arr_14"]
+    gamma = nn_data["arr_15"]
+    mean = nn_data["arr_16"]
+    inv_stddev = nn_data["arr_17"]
+
+    x_with_mean = input_out_layer - mean
+    output_layer_output = np.sign((gamma * x_with_mean * inv_stddev) + beta).astype(int)
     
     # update results
-    output_from_nn = np.argmax(out_layer_output)
+    output_from_nn = np.argmax(output_layer_output)
     correct_output = test_set[1][test_image]
     if output_from_nn == correct_output:
         correct_images += 1
