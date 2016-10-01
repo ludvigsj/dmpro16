4ae02df44a7c139917ded8aa4bb038d93754342d
Olav Markussen <olavmarkussen@hotmail.no>
2016-10-01 20:52:08 +0200

    Add function to compute a layer, given parameters

--- bnn/nn.py
+++ bnn/nn.py
@@ -28,6 +28,16 @@ with gzip.open('mnist.pkl.gz', 'rb') as f:
 # test_set contains 10000 images
 total_images = 10000
 correct_images = 0
+
+def compute_layer(previous_activation, weights, beta, gamma, mean, inv_stddev):
+    activation = np.dot(previous_activation, weights)
+    
+    activation_with_mean = activation - mean
+    first_layer_output = np.sign((gamma * activation_with_mean * inv_stddev) +
+            beta).astype(int)
+
+    return first_layer_output
+
 for test_image in range(total_images):
     image_vector = test_set[0][test_image]
     for i in range(784):
@@ -37,50 +47,32 @@ for test_image in range(total_images):
             image_vector[i] = -1
     
     # compute first hidden layer (1024)
-    l1_size = 1024
     
-    first_layer_in = np.dot(image_vector, nn_first_synapses)
-    
-    beta = nn_data["arr_2"]
-    gamma = nn_data["arr_3"]
-    mean = nn_data["arr_4"]
-    inv_stddev = nn_data["arr_5"]
-    
-    x_with_mean = first_layer_in - mean
-    first_layer_output = (gamma * x_with_mean * inv_stddev) + beta
-    #first_layer_output = np.tanh(first_layer_output)
-    first_layer_output = np.sign(first_layer_output).astype(int)
-    
+    layer_1 = compute_layer(image_vector,
+            nn_first_synapses,
+            nn_data["arr_2"],
+            nn_data["arr_3"],
+            nn_data["arr_4"],
+            nn_data["arr_5"])
+
     # compute second hidden layer (128)
-    l2_size = 128
-    input_second_layer = np.dot(first_layer_output, nn_second_synapses)
-    
-    beta = nn_data["arr_8"]
-    gamma = nn_data["arr_9"]
-    mean = nn_data["arr_10"]
-    inv_stddev = nn_data["arr_11"]
-    
-    x_with_mean = input_second_layer - mean
-    second_layer_output = (gamma * x_with_mean * inv_stddev) + beta
-    second_layer_output = np.sign(second_layer_output).astype(int)
-    #second_layer_output = np.tanh(second_layer_output)
+    layer_2 = compute_layer(layer_1,
+            nn_second_synapses,
+            nn_data["arr_8"],
+            nn_data["arr_9"],
+            nn_data["arr_10"],
+            nn_data["arr_11"])
 
     # compute output layer (10)
-    out_size = 10
-    input_out_layer = np.dot(second_layer_output, nn_out_synapses)
-    
-    beta = nn_data["arr_14"]
-    gamma = nn_data["arr_15"]
-    mean = nn_data["arr_16"]
-    inv_stddev = nn_data["arr_17"]
+    layer_3 = compute_layer(layer_2,
+            nn_out_synapses,
+            nn_data["arr_14"],
+            nn_data["arr_15"],
+            nn_data["arr_16"],
+            nn_data["arr_17"])
 
-    x_with_mean = input_out_layer - mean
-    output_layer_output = np.sign((gamma * x_with_mean * inv_stddev) + beta).astype(int)
-    
-    # update results
-    output_from_nn = np.argmax(output_layer_output)
+    output_from_nn = np.argmax(layer_3)
     correct_output = test_set[1][test_image]
     if output_from_nn == correct_output:
         correct_images += 1
         print("{}/{} correct images".format(correct_images, test_image+1))
-
