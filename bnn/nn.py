863797ab9c5523544f4923f123472545e9a01604
Ilse Gerda Visser <irxess@gmail.com>
2016-10-01 16:34:37 +0200

    Use an activation function, now 96% accuracy

--- bnn/nn.py
+++ bnn/nn.py
@@ -2,6 +2,13 @@ import cPickle
 import gzip
 import numpy as np
 
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
 # load ANN data
 nn_data = np.load("2-layer.npz")
 
@@ -17,7 +24,10 @@ correct_images = 0
 for test_image in range(total_images):
     image_vector = test_set[0][test_image]
     for i in range(784):
-        image_vector[i] = image_vector[i]*255
+        if image_vector[i] > 0.6: # colors are not linear
+            image_vector[i] = 255
+        else:
+            image_vector[i] = 0
     
     # compute first hidden layer (1024)
     l1_size = 1024
@@ -26,13 +36,14 @@ for test_image in range(total_images):
     first_layer_in = np.dot(image_vector, nn_data["arr_0"])
     
     beta = nn_data["arr_2"]
-    inv_stddev = nn_data["arr_3"]
+    gamma = nn_data["arr_3"]
     mean = nn_data["arr_4"]
-    gamma = nn_data["arr_5"]
+    inv_stddev = nn_data["arr_5"]
     
     x_with_mean = first_layer_in - mean
     first_layer_output = (gamma * x_with_mean * inv_stddev) + beta
-    
+    # tanh or sigmoid
+    first_layer_output = np.tanh(first_layer_output)
     
     # compute second hidden layer (128)
     l2_size = 128
@@ -40,14 +51,17 @@ for test_image in range(total_images):
     second_layer_output = np.empty(l2_size)
     
     beta = nn_data["arr_8"]
-    inv_stddev = nn_data["arr_9"]
+    gamma = nn_data["arr_9"]
     mean = nn_data["arr_10"]
-    gamma = nn_data["arr_11"]
+    inv_stddev = nn_data["arr_11"]
     
     x_with_mean = input_second_layer - mean
     second_layer_output = (gamma * x_with_mean * inv_stddev) + beta
-    
-    
+    # some activation function?
+    #second_layer_output = softmax(second_layer_output)
+    #second_layer_output = sigmoid(second_layer_output)
+    second_layer_output = np.tanh(second_layer_output)
+
     # compute output layer (10)
     out_size = 10
     input_out_layer = np.zeros(out_size)
@@ -58,10 +72,10 @@ for test_image in range(total_images):
             current_input += second_layer_output[synapse_used] * nn_data["arr_12"][synapse_used][neuron]
     
     # apply batchnorm
-        beta = nn_data["arr_2"][neuron]
-        inv_stddev = nn_data["arr_3"][neuron]
-        mean = nn_data["arr_4"][neuron]
-        gamma = nn_data["arr_5"][neuron]
+        beta = nn_data["arr_14"][neuron]
+        gamma = nn_data["arr_15"][neuron]
+        mean = nn_data["arr_16"][neuron]
+        inv_stddev = nn_data["arr_17"][neuron]
     
         batchnorm = lambda x: gamma * (x - mean) * inv_stddev + beta
         out_layer_output[neuron] = batchnorm( current_input )
@@ -71,5 +85,5 @@ for test_image in range(total_images):
     correct_output = test_set[1][test_image]
     if output_from_nn == correct_output:
         correct_images += 1
-    print("{}/{} correct images".format(correct_images, test_image+1))
+        print("{}/{} correct images".format(correct_images, test_image+1))
 
