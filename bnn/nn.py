2dbe98759c46db26cba198505efa8f968d25e6f1
Ilse Gerda Visser <irxess@gmail.com>
2016-10-03 20:20:32 +0200

    Make batchnorm computation easier

--- bnn/nn.py
+++ bnn/nn.py
@@ -2,21 +2,13 @@ import cPickle
 import gzip
 import numpy as np
 
-def binsign(x):
-    return 1 if x>0 else 0
-binsign = np.vectorize(binsign, otypes=[np.int])
-
-def xnor(a,b):
-    return np.logical_not(np.logical_xor(a,b))
-
-def xnorsum(v,w):
-    result = np.empty(w.shape[0])
-    for i in range(w.shape[0]):
-        result[i] = np.sum(xnor(v,w[i]))
-    return result
 
 # load ANN data
 nn_data = np.load("2-layer.npz")
+
+def binsign(x):
+    return 1 if x>0 else 0
+binsign = np.vectorize(binsign, otypes=[np.int])
 
 # Make synapses binary
 nn_first_synapses = binsign(nn_data["arr_0"]).astype(int)
@@ -34,6 +26,48 @@ with gzip.open('mnist.pkl.gz', 'rb') as f:
 # test_set[1] is the expected output
 total_images = 10000 # test_set contains 10000 images
 correct_images = 0
+
+def simplify_batchnorm(beta, gamma, mean, inv_stddev, previous_activation_shape):
+    multiplier = gamma*inv_stddev
+    batchnorm_array = (0.5 * (mean + previous_activation_shape - 
+                          beta/(multiplier) )).astype(int)
+    def is_growing(a):
+        if a > 0:
+            return 1
+        else:
+            return -1
+    is_growing = np.vectorize(is_growing, otypes=[np.int])
+    growth = is_growing(multiplier)
+    return batchnorm_array, growth
+
+batchnorm1, g1 = simplify_batchnorm(nn_data["arr_2"], nn_data["arr_3"],
+                                    nn_data["arr_4"], nn_data["arr_5"],
+                                    784)
+batchnorm2, g2 = simplify_batchnorm(nn_data["arr_8"], nn_data["arr_9"],
+                                    nn_data["arr_10"], nn_data["arr_11"],
+                                    batchnorm1.shape[0])
+batchnorm3, g3 = simplify_batchnorm(nn_data["arr_14"], nn_data["arr_15"],
+                                    nn_data["arr_16"], nn_data["arr_17"],
+                                    batchnorm2.shape[0])
+
+def xnor(a,b):
+    return np.logical_not(np.logical_xor(a,b))
+
+def xnorsum(v,w):
+    result = np.empty(w.shape[0])
+    for i in range(w.shape[0]):
+        result[i] = np.sum(xnor(v,w[i]))
+    return result
+
+def do_batchnorm(xnorsum, batchnorm):
+    if xnorsum > batchnorm:
+        return 1
+    else:
+        return 0
+do_batchnorm = np.vectorize(do_batchnorm, otypes=[np.int])
+
+def alt_compute_layer(previous_activation, weights, batchnorm, growth):
+    return do_batchnorm( xnorsum(previous_activation, weights)*growth, batchnorm )
 
 def compute_layer(previous_activation, weights, beta, gamma, mean, inv_stddev):
     #activation = np.dot(previous_activation, weights)
@@ -54,29 +88,33 @@ for test_image in range(total_images):
         else:
             image_vector[i] = 0
     
+    layer_1 = alt_compute_layer(image_vector, nn_first_synapses, batchnorm1, g1)
+    layer_2 = alt_compute_layer(layer_1, nn_second_synapses, batchnorm2, g2)
+    layer_3 = alt_compute_layer(layer_2, nn_out_synapses, batchnorm3, g3)
+
     # compute first hidden layer (1024) 
-    layer_1 = compute_layer(image_vector,
-            nn_first_synapses,
-            nn_data["arr_2"],
-            nn_data["arr_3"],
-            nn_data["arr_4"],
-            nn_data["arr_5"])
+    #layer_1 = compute_layer(image_vector,
+    #        nn_first_synapses,
+    #        nn_data["arr_2"],
+    #        nn_data["arr_3"],
+    #        nn_data["arr_4"],
+    #        nn_data["arr_5"])
 
     # compute second hidden layer (128)
-    layer_2 = compute_layer(layer_1,
-            nn_second_synapses,
-            nn_data["arr_8"],
-            nn_data["arr_9"],
-            nn_data["arr_10"],
-            nn_data["arr_11"])
+    #layer_2 = compute_layer(layer_1,
+    #        nn_second_synapses,
+    #        nn_data["arr_8"],
+    #        nn_data["arr_9"],
+    #        nn_data["arr_10"],
+    #        nn_data["arr_11"])
 
     # compute output layer (10)
-    layer_3 = compute_layer(layer_2,
-            nn_out_synapses,
-            nn_data["arr_14"],
-            nn_data["arr_15"],
-            nn_data["arr_16"],
-            nn_data["arr_17"])
+    #layer_3 = compute_layer(layer_2,
+    #        nn_out_synapses,
+    #        nn_data["arr_14"],
+    #        nn_data["arr_15"],
+    #        nn_data["arr_16"],
+    #        nn_data["arr_17"])
 
     output_from_nn = np.argmax(layer_3)
     correct_output = test_set[1][test_image]
