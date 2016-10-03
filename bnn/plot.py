f46a3a0c7e9a6eeddc63510c8e6155a31389c175
Ilse Gerda Visser <irxess@gmail.com>
2016-10-03 13:22:50 +0200

    Plot the output of batchnorm against the possible inputs for batchnorm

--- bnn/plot.py
+++ bnn/plot.py
@@ -1,60 +1,70 @@
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 allparams = np.load("2-layer.npz")
 
 nof_neurons = 1024
-#nof_neurons = 30
+nof_neurons = 14
 nof_synapses = 784
-nof_synapses = 15
-neuron = 8
-synapse_used = 0
+#nof_synapses = 15
 
 red_neurons = []
 red_neuron_outputs = []
 blue_neurons = []
 blue_neuron_outputs = []
+neurons = []
+neuron_outputs = []
+
+def binsign(x):
+    return 1 if x>0 else -1
+binsign = np.vectorize(binsign, otypes=[np.int])
+
+nn_first_synapses = binsign(allparams["arr_0"]).astype(int)
+nn_second_synapses = binsign(allparams["arr_6"]).astype(int)
+nn_out_synapses = binsign(allparams["arr_12"]).astype(int)
 
 sign = lambda x: -1 if x < 0 else 1 if x > 0 else 0
-#neuron_list = [0, 11, 3, 1]
-#neuron_list = [3]
 
 for neuron in range(nof_neurons):
-#for neuron in neuron_list:
- for synapse_used in range(nof_synapses):
-  beta = allparams["arr_2"][neuron]
-  inv_stddev = allparams["arr_3"][neuron]
-  mean = allparams["arr_4"][neuron]
-#  mean = 0
-  gamma = allparams["arr_5"][neuron]
-  binary_synapses = sign( allparams["arr_0"][synapse_used][neuron] )
-  # binary_synapses = allparams["arr_0"][synapse_used][neuron]
-  binary_weights = 1 # bias values can be ignored
+  neurons = []
+  neuron_outputs = []
+  #for activation in range(-784, 784, 2):
+  for activation in range(0, 784, 1):
+    beta = allparams["arr_2"][neuron]
+    inv_stddev = allparams["arr_3"][neuron]
+    mean = allparams["arr_4"][neuron]
+    gamma = allparams["arr_5"][neuron]
 
-  binary_stuff = binary_weights * binary_synapses
-  batchnorm = lambda x: gamma * (x - mean) * inv_stddev + beta
-  neuron_output = sign( batchnorm(binary_stuff) )
-  # neuron_output = ( batchnorm(binary_stuff) )
+    batchnorm = lambda x: gamma * ((x*2)-784 - mean) * inv_stddev + beta
+    neurons.append(activation)
+    neuron_outputs.append( batchnorm(activation) )
+   
+    #red_neuron_outputs.append(neuron_output)
+    #blue_neuron_outputs.append(neg_neuron_output)
+    #red_neurons.append(binary_stuff)
+    #blue_neurons.append(neg_input)
+    #neurons.append(neuron)
+    
+    #synapse_used = synapse_used + neuron * nof_synapses
+    #if binary_stuff <= 0:
+    #  blue_neurons.append(neuron)
+      #blue_neurons.append(synapse_used)
+    #  blue_neuron_outputs.append(neuron_output)
+    #else:
+    #  red_neurons.append(neuron)
+      #red_neurons.append(synapse_used)
+    #  red_neuron_outputs.append(neuron_output)
+
   
-  #synapse_used = synapse_used + neuron * nof_synapses
-  if binary_stuff < 0:
-     # blue_neurons.append(neuron)
-      blue_neurons.append(synapse_used)
-      blue_neuron_outputs.append(neuron_output)
-  else:
-      # red_neurons.append(neuron)
-      red_neurons.append(synapse_used)
-      red_neuron_outputs.append(neuron_output)
-
-  
-# plot binary stuff versus neuron output
-# x-axis neuron number
-# y-axis neuron output
-# color different depending on binary_stuff
-
+  plt.figure(neuron)
 #plt.plot(np.array(neuron_number), np.array(neuron_outputs), 'o', c=np.array(colors))
-#plt.plot(neuron_number, neuron_outputs, 'o', c=colors)
-plt.plot(blue_neurons, blue_neuron_outputs, 'bo', red_neurons, red_neuron_outputs, 'ro')
-plt.show()
-
-
+  plt.plot(neurons, neuron_outputs, '-')
+#plt.plot(neurons, blue_neurons, 'bs', neurons, blue_neuron_outputs, 'bo', neurons, red_neurons, 'rs', neurons, red_neuron_outputs, 'ro')
+#plt.plot(blue_neurons, blue_neuron_outputs, 'bo', red_neurons, red_neuron_outputs, 'ro')
+#plt.show()
+  filename = "plot2/" + str(neuron) + ".png"
+  plt.savefig(filename, bbox_inches='tight')
+  print(filename)
+  plt.close()
