78787bd9e905faeb7689efc1d5d41e2db7078f55
Olav Markussen <olavmarkussen@hotmail.no>
2016-10-13 07:37:35 +0200

    Use 3-layer net in nn.py

--- bnn/nn.py
+++ bnn/nn.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 # load ANN data
-nn_data = np.load("2-layer.npz")
+nn_data = np.load("3-layer.npz")
 
 def binsign(x):
     return 1 if x>0 else 0
@@ -13,10 +13,12 @@ binsign = np.vectorize(binsign, otypes=[np.int])
 # Make synapses binary
 nn_first_synapses = binsign(nn_data["arr_0"]).astype(int)
 nn_second_synapses = binsign(nn_data["arr_6"]).astype(int)
-nn_out_synapses = binsign(nn_data["arr_12"]).astype(int)
+nn_third_synapses = binsign(nn_data["arr_12"]).astype(int)
+nn_out_synapses = binsign(nn_data["arr_18"]).astype(int)
 # and easier to work with
 nn_first_synapses = np.swapaxes(nn_first_synapses,0,1)
 nn_second_synapses = np.swapaxes(nn_second_synapses,0,1)
+nn_third_synapses = np.swapaxes(nn_third_synapses,0,1)
 nn_out_synapses = np.swapaxes(nn_out_synapses,0,1)
 
 # original dataset is 0-255, this one uses floats
@@ -49,7 +51,9 @@ batchnorm2, g2 = simplify_batchnorm(nn_data["arr_8"], nn_data["arr_9"],
 batchnorm3, g3 = simplify_batchnorm(nn_data["arr_14"], nn_data["arr_15"],
                                     nn_data["arr_16"], nn_data["arr_17"],
                                     batchnorm2.shape[0])
-
+batchnorm4, g4 = simplify_batchnorm(nn_data["arr_20"], nn_data["arr_21"],
+                                    nn_data["arr_22"], nn_data["arr_23"],
+                                    batchnorm2.shape[0])
 def xnor(a,b):
     return np.logical_not(np.logical_xor(a,b))
 
@@ -69,17 +73,6 @@ do_batchnorm = np.vectorize(do_batchnorm, otypes=[np.int])
 def alt_compute_layer(previous_activation, weights, batchnorm, growth):
     return do_batchnorm( xnorsum(previous_activation, weights)*growth, batchnorm )
 
-def compute_layer(previous_activation, weights, beta, gamma, mean, inv_stddev):
-    #activation = np.dot(previous_activation, weights)
-    activation = xnorsum(previous_activation, weights)
-    activation_with_mean = (activation*2)-previous_activation.shape[0] - mean
-    #current_layer_output = np.sign((gamma * activation_with_mean * inv_stddev) +
-    #        beta).astype(int)
-    current_layer_output = binsign((gamma * activation_with_mean * inv_stddev) +
-            beta).astype(int)
-
-    return current_layer_output
-
 for test_image in range(total_images):
     image_vector = np.empty(784, dtype=int)
     for i in range(784):
@@ -90,9 +83,10 @@ for test_image in range(total_images):
     
     layer_1 = alt_compute_layer(image_vector, nn_first_synapses, batchnorm1, g1)
     layer_2 = alt_compute_layer(layer_1, nn_second_synapses, batchnorm2, g2)
-    layer_3 = alt_compute_layer(layer_2, nn_out_synapses, batchnorm3, g3)
+    layer_3 = alt_compute_layer(layer_2, nn_third_synapses, batchnorm3, g3)
+    layer_4 = alt_compute_layer(layer_3, nn_out_synapses, batchnorm4, g4)
 
-    output_from_nn = np.argmax(layer_3)
+    output_from_nn = np.argmax(layer_4)
     correct_output = test_set[1][test_image]
     if output_from_nn == correct_output:
         correct_images += 1
