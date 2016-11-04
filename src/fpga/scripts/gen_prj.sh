753d31f4126c8272bf9b8ef09161ac43dc76bb76
Ludvig Jordet <ludvig.jordet@gmail.com>
2016-11-04 16:33:07 +0100

    Revert "clean up surr"
    This reverts commit 9e6dc4b8125dc92729b39e3477104b12dac116ec.

--- src/fpga/scripts/gen_prj.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-topdir=../bnn
-(find -L "${topdir}/verilog" -name '*.v' | xargs -n1 -I{} echo "v work" {})
--- src/fpga/scripts/setup_workdir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-workdir=../vivado-workdir
-
-mkdir -p ${workdir}
-## Always regenerate the project file, in case new source files have been added.
-./gen_prj.sh > ${workdir}/sim-prj.prj;
