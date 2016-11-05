5154cb4c111a79dfd6abe1743e24183911852b6f
Eirik Rognø <eirik.rogno@gmail.com>
2016-11-05 11:37:13 +0100

    Optimize so the algorithm doesn't need to go through the whole image, a bit hacky

--- src/fpga/corners/Corners.scala
+++ src/fpga/corners/Corners.scala
@@ -6,6 +6,8 @@ import java.io.File
 import java.awt.Color
 
 class CornersModule extends Module {
+  // x0, y0 always outputs left corner. x1, y1 always outputs right corner.
+
   val io = new Bundle {
     val pxin = UInt(INPUT, width = 16)
     val x0= UInt(OUTPUT, width = 16)
@@ -17,8 +19,8 @@ class CornersModule extends Module {
     val adr = UInt(OUTPUT, width = 20)
   }
 
-  val x, y, firstx, firsty, secondy2,secondx2= Reg(init=UInt(0, width = 16));
-  val secondx1, secondy1 = Reg(init=UInt(999, width = 16));
+  val x, y, firstx, firsty, secondy2,secondx2, rightestx, rightesty = Reg(init=UInt(0, width = 16));
+  val secondx1, secondy1, leftestx, leftesty = Reg(init=UInt(999, width = 16));
   val first = Reg(init=Bool(false));
   val x0temp, y0temp, x1temp, y1temp = Reg(init=UInt(0, width = 16));
 
@@ -36,6 +38,8 @@ class CornersModule extends Module {
     when(x === 639.U){
       y := y + 1.U;
       x := 0.U;
+      rightestx := 0.U;
+      leftestx := 999.U
     }.otherwise{
       x := x + 1.U
     }
@@ -60,53 +64,96 @@ class CornersModule extends Module {
     when(!first){
       firstx := x;
       firsty := y;
-      secondx1 := x;
       first := Bool(true);
     }.otherwise{
       when(x < secondx1){
         secondx1 := x;
         secondy1 := y;
-      }.elsewhen(x > secondx2){
+      }
+
+      when(x > secondx2){
         secondx2 := x;
         secondy2 := y;
+      }
+
+      when(x < leftestx){
+        leftestx := x;
+        leftesty := y;
+      }
+
+      when(x > rightestx){
+        rightestx := x;
+        rightesty := y;
       }
     }
   }
 
-  when(secondy2 < secondy1){
-    when(secondx2 < firstx){
-      x0temp := firstx;
-      y0temp := firsty;
-      x1temp := secondx2;
-      y1temp := secondy2;
-    }.otherwise{
-      x0temp := firstx;
-      y0temp := firsty;
-      x1temp := secondx2;
-      y1temp:= secondy2;
+  when(x === 479.U){
+    when(leftestx === firstx){
+      when(leftesty > ( firsty + 5.U)){
+        x0temp := firstx;
+        y0temp := firsty;
+        x1temp := secondx2;
+        y1temp := secondy2;
+
+        done := 1.U;
+      }
     }
-  }.otherwise{
-    when(secondx1 <= firstx){
-      x0temp := firstx;
-      y0temp := firsty;
-      x1temp := secondx1;
-      y1temp := secondy1;
-    }.otherwise{
-      x0temp := firstx;
-      y0temp := firsty;
-      x1temp := secondx1;
-      y1temp := secondy1;
+
+
+    when(rightestx < secondx2){
+      when(rightesty > (secondy2 + 5.U)){
+        x0temp := firstx;
+        y0temp := firsty;
+        x1temp := secondx2;
+        y1temp := secondy2;
+
+        done := 1.U;
+      }
+    }
+
+    when(leftestx > secondx1){
+      when(leftesty > (secondy1 + 5.U)){
+        x0temp := secondx1;
+        y0temp := secondy1;
+        x1temp := firstx;
+        y1temp := firsty;
+
+        done := 1.U;
+      }
     }
   }
 
 }
 
 class CornersModuleTests(c: CornersModule) extends Tester(c) {
-  val photo = ImageIO.read(new File("../../../test_images/sudoku2_right_40deg.bmp"));
-  var x,y : Int = 0;
+  var x,y, x0,y0,x1,y1 : Int = 0;
   var done : Boolean = false;
 
+  /*val imagename : String = "sudoku2_middle.bmp";
+  x0 = 180;
+  y0 = 108;
+  x1 = 439;
+  y1 = 108;*/
+
+
+  /*val imagename : String = "sudoku2_right_40deg.bmp";
+  x0 = 392;
+  y0 = 60;
+  x1 = 592;
+  y1 = 226;*/
+
+
+  val imagename : String = "sudoku2_left_-39deg.bmp";
+  x0 = 27;
+  y0 = 207;
+  x1 = 228;
+  y1 = 44;
+
+
   poke(c.io.enable,1);
+
+  val photo = ImageIO.read(new File("../../../test_images/" + imagename));
 
   while(!done){
     if((new Color(photo.getRGB(x, y))).getGreen() >= 128){
@@ -126,28 +173,14 @@ class CornersModuleTests(c: CornersModule) extends Tester(c) {
     if(peek(c.io.done) == 1) {
       done = true;
     }
+
   }
 
-  /*for (y <- 0 until 480)
-  {
-    for (x <- 0 until 640)
-    {
-      if((new Color(photo.getRGB(x, y))).getGreen() >= 128){
-        poke(c.io.pxin, 1);
-      }else{
-        poke(c.io.pxin, 0);
-      }
-      step(1)
-    }
-  }*/
-
   expect(c.io.done, 1)
-  expect(c.io.x0, 392)
-  expect(c.io.y0, 60)
-  expect(c.io.x1, 592)
-  expect(c.io.y1, 226)
-
-
+  expect(c.io.x0, x0)
+  expect(c.io.y0, y0)
+  expect(c.io.x1, x1)
+  expect(c.io.y1, y1)
 
 }
 
