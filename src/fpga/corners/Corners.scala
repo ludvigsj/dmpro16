cab15a4e6aebf04586b8a8bf07a2b4427e841ee1
Eirik Rognø <eirik.rogno@gmail.com>
2016-11-10 15:40:42 +0100

    Works with photographs when the right corner is highest

--- src/fpga/corners/Corners.scala
+++ src/fpga/corners/Corners.scala
@@ -4,12 +4,16 @@ import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 import java.io.File
 import java.awt.Color
+import java.awt.image.Raster
 
 class CornersModule extends Module {
   // x0, y0 always outputs left corner. x1, y1 always outputs right corner.
 
   val io = new Bundle {
-    val pxin = UInt(INPUT, width = 16)
+    val pxin = UInt(INPUT, width = 1)
+    val pxin1 = UInt(INPUT, width = 1)
+    val pxin2 = UInt(INPUT, width = 1)
+    val pxin3 = UInt(INPUT, width = 1)
     val x0= UInt(OUTPUT, width = 16)
     val x1= UInt(OUTPUT, width = 16)
     val y0= UInt(OUTPUT, width = 16)
@@ -21,7 +25,7 @@ class CornersModule extends Module {
 
   val x, y, firstx, firsty, secondy2,secondx2, rightestx, rightesty = Reg(init=UInt(0, width = 16));
   val secondx1, secondy1, leftestx, leftesty = Reg(init=UInt(999, width = 16));
-  val first = Reg(init=Bool(false));
+  val first, firstFound = Reg(init=Bool(false));
   val x0temp, y0temp, x1temp, y1temp = Reg(init=UInt(0, width = 16));
 
   val done = Reg(init=UInt(0, width = 1));
@@ -51,76 +55,97 @@ class CornersModule extends Module {
     first := Bool(false);
     secondx1 := 999.U;
     secondx2 := 0.U;
+    rightestx := 0.U;
+    leftestx := 999.U;
   }
 
-  when(y === 479.U){
-    when(x === 639.U){
-      done := 1.U;
-    }
+  when(y === 479.U && x === 639.U){
+    done := 1.U;
   }
 
+  when(first && !firstFound &&
+    io.pxin === UInt(0) &&
+    io.pxin1 === UInt(0) &&
+    io.pxin2 === UInt(0) &&
+    io.pxin3 === UInt(0) ){
+      firstx := x - 1.U;
+      firsty := y;
+      firstFound := Bool(true);
+  }
 
   when(io.pxin === UInt(1)){
     when(!first){
-      firstx := x;
-      firsty := y;
       first := Bool(true);
-    }.otherwise{
-      when(x < secondx1){
-        secondx1 := x;
-        secondy1 := y;
-      }
-
-      when(x > secondx2){
-        secondx2 := x;
-        secondy2 := y;
-      }
-
-      when(x < leftestx){
-        leftestx := x;
-        leftesty := y;
-      }
-
-      when(x > rightestx){
-        rightestx := x;
-        rightesty := y;
-      }
     }
+
+    when(firstFound){
+
+      /*when(x > secondx2 &&
+        io.pxin1 === UInt(0) &&
+        io.pxin2 === UInt(0) &&
+        io.pxin3 === UInt(0)){
+
+          secondx2 := x;
+          secondy2 := y;
+      }
+
+      when(x > rightestx &&
+        io.pxin1 === UInt(0) &&
+        io.pxin2 === UInt(0) &&
+        io.pxin3 === UInt(0)){
+
+          rightestx := x;
+          rightesty := y;
+      }*/
+
+      when(x < secondx1 &&
+        io.pxin1 === UInt(1) &&
+        io.pxin2 === UInt(1) &&
+        io.pxin3 === UInt(1)){
+          secondx1 := x;
+          secondy1 := y;
+      }
+
+      when(x < leftestx &&
+        io.pxin1 === UInt(1) &&
+        io.pxin2 === UInt(1) &&
+        io.pxin3 === UInt(1)){
+          leftestx := x;
+          leftesty := y;
+      }
+
+    }
+
   }
 
-  when(x === 479.U){
-    when(leftestx === firstx){
-      when(leftesty > ( firsty + 5.U)){
-        x0temp := firstx;
-        y0temp := firsty;
-        x1temp := secondx2;
-        y1temp := secondy2;
+  when(x === 639.U){
+    when(leftestx === firstx && leftesty > ( firsty + 5.U)){
+      x0temp := firstx;
+      y0temp := firsty;
+      x1temp := secondx2;
+      y1temp := secondy2;
 
-        done := 1.U;
-      }
+      done := 1.U;
+
     }
 
 
-    when(rightestx < secondx2){
-      when(rightesty > (secondy2 + 5.U)){
-        x0temp := firstx;
-        y0temp := firsty;
-        x1temp := secondx2;
-        y1temp := secondy2;
+    when(rightestx < secondx2 && rightesty > (secondy2 + 5.U)){
+      x0temp := firstx;
+      y0temp := firsty;
+      x1temp := secondx2;
+      y1temp := secondy2;
 
-        done := 1.U;
-      }
+      done := 1.U;
     }
 
-    when(leftestx > secondx1){
-      when(leftesty > (secondy1 + 5.U)){
-        x0temp := secondx1;
-        y0temp := secondy1;
-        x1temp := firstx;
-        y1temp := firsty;
+    when(leftestx > secondx1 && leftesty > (secondy1 + 5.U)){
+      x0temp := secondx1;
+      y0temp := secondy1;
+      x1temp := firstx;
+      y1temp := firsty;
 
-        done := 1.U;
-      }
+      done := 1.U;
     }
   }
 
@@ -130,37 +155,55 @@ class CornersModuleTests(c: CornersModule) extends Tester(c) {
   var x,y, x0,y0,x1,y1 : Int = 0;
   var done : Boolean = false;
 
-  /*val imagename : String = "sudoku2_middle.bmp";
-  x0 = 180;
-  y0 = 108;
-  x1 = 439;
-  y1 = 108;*/
+  /*val imagename : String = "foto3.jpg";
+  x0 = 176;
+  y0 = 86;
+  x1 = 504;
+  y1 = 60;*/
 
+  /*val imagename : String = "foto6.jpg";
+  x0 = 155;
+  y0 = 70;
+  x1 = 477;
+  y1 = 99;*/
 
-  /*val imagename : String = "sudoku2_right_40deg.bmp";
-  x0 = 392;
-  y0 = 60;
-  x1 = 592;
-  y1 = 226;*/
-
-
-  val imagename : String = "sudoku2_left_-39deg.bmp";
-  x0 = 27;
-  y0 = 207;
-  x1 = 228;
-  y1 = 44;
-
+  val imagename : String = "foto2.jpg";
+  x0 = 193;
+  y0 = 92;
+  x1 = 501;
+  y1 = 89;
 
   poke(c.io.enable,1);
 
   val photo = ImageIO.read(new File("../../../test_images/" + imagename));
 
   while(!done){
-    if((new Color(photo.getRGB(x, y))).getGreen() >= 128){
+    if((new Color(photo.getRGB(x, y))).getGreen()  >= 128){
       poke(c.io.pxin, 1);
     }else{
       poke(c.io.pxin, 0);
     }
+
+    if(x <= 636){
+      if((new Color(photo.getRGB(x+1, y))).getGreen()  >= 128){
+        poke(c.io.pxin1, 1);
+      }else{
+        poke(c.io.pxin1, 0);
+      }
+      if((new Color(photo.getRGB(x+2, y))).getGreen()  >= 128){
+        poke(c.io.pxin2, 1);
+      }else{
+        poke(c.io.pxin2, 0);
+      }
+      if((new Color(photo.getRGB(x+3, y))).getGreen()  >= 128){
+        poke(c.io.pxin3, 1);
+      }else{
+        poke(c.io.pxin3, 0);
+      }
+
+
+    }
+
     step(1)
 
     if(x == 639){
@@ -176,7 +219,6 @@ class CornersModuleTests(c: CornersModule) extends Tester(c) {
 
   }
 
-  expect(c.io.done, 1)
   expect(c.io.x0, x0)
   expect(c.io.y0, y0)
   expect(c.io.x1, x1)
