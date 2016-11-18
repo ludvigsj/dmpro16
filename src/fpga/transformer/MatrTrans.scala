8172a591b886517501a60d3929e6439890c23d03
Ludvig Jordet <ludvig.jordet@gmail.com>
2016-11-18 17:58:49 +0100

    zext other coordinate also

--- src/fpga/transformer/MatrTrans.scala
+++ src/fpga/transformer/MatrTrans.scala
@@ -22,7 +22,7 @@ class MatrTrans extends Module {
 
 
     io.srcX := (((io.x1-io.x0)*io.dstX).zext - ((io.y1-io.y0)*io.dstY).zext)/SInt(252) + io.x0
-    io.srcY := ((io.y1-io.y0)*io.dstX + (io.x1-io.x0)*io.dstY)/UInt(252) + io.y0
+    io.srcY := (((io.y1-io.y0)*io.dstX).zext + ((io.x1-io.x0)*io.dstY).zext)/SInt(252) + io.y0
 }
 
 class MatrTransTests(c: MatrTrans) extends Tester(c) {
