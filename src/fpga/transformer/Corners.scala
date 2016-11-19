f404d78b49bcf30ec3983215630b7f920fada893
Eirik Rognø <eirik.rogno@gmail.com>
2016-11-19 13:51:18 +0100

    Works with all photos except foto4.gray which has a random white pixel that messes it up

--- src/fpga/transformer/Corners.scala
+++ src/fpga/transformer/Corners.scala
@@ -2,10 +2,7 @@ package SudoKu
 
 import Chisel._
 
-import javax.imageio.ImageIO
-import java.awt.image.BufferedImage
-import java.io.File
-import java.awt.Color
+import java.nio.file.{Files, Paths}
 
 class CornersModule extends Module {
   // x0, y0 always outputs left corner. x1, y1 always outputs right corner.
@@ -23,7 +20,7 @@ class CornersModule extends Module {
 
   val x, y, firstx, firsty, secondy2,secondx2, rightestx, rightesty = Reg(init=UInt(0, width = 16));
   val secondx1, secondy1, leftestx, leftesty = Reg(init=UInt(999, width = 16));
-  val first, firstFound = Reg(init=Bool(false));
+  val firstLeft, firstRight, firstFound = Reg(init=Bool(false));
   val x0temp, y0temp, x1temp, y1temp = Reg(init=UInt(0, width = 16));
   val pxin1 = Reg(next=io.pxin);
   val pxin2 = Reg(next=pxin1);
@@ -40,20 +37,23 @@ class CornersModule extends Module {
   io.y1 := y1temp;
 
   when(io.enable){
-    when(x === 639.U){
-      y := y + 1.U;
-      x := 0.U;
-      rightestx := 0.U;
-      leftestx := 999.U
-    }.otherwise{
-      x := x + 1.U
+    when(!done){
+      when(x === 639.U){
+        y := y + 1.U;
+        x := 0.U;
+        rightestx := 0.U;
+        leftestx := 999.U
+      }.otherwise{
+        x := x + 1.U
+      }
     }
   }.otherwise{
     x := 0.U;
     y := 0.U;
 
     done := Bool(false);
-    first := Bool(false);
+    firstLeft := Bool(false);
+    firstRight := Bool(false);
     secondx1 := 999.U;
     secondx2 := 0.U;
     rightestx := 0.U;
@@ -64,7 +64,7 @@ class CornersModule extends Module {
     done := Bool(true);
   }
 
-  when(first && !firstFound &&
+  when(firstRight && !firstFound &&
     pxin3 === UInt(0) &&
     pxin2 === UInt(0) &&
     pxin1 === UInt(0) &&
@@ -72,133 +72,156 @@ class CornersModule extends Module {
       firstx := x - 1.U;
       firsty := y;
       firstFound := Bool(true);
+  }.elsewhen(firstLeft && !firstFound){
+    firstx := x;
+    firsty := y;
+    firstFound := Bool(true);
   }
 
   when(pxin3 === UInt(1)){
-    when(!first){
-      first := Bool(true);
+    when(!firstLeft && !firstRight){
+      when(x < 640.U/2.U){
+        firstLeft := Bool(true)
+      }.otherwise{
+        firstRight := Bool(true)
+      }
     }
 
     when(firstFound){
 
-      /*when(x > secondx2 &&
-        pxin2 === UInt(0) &&
-        pxin1 === UInt(0) &&
-        io.pxin === UInt(0)){
+      when(firstLeft){
+        when(x > secondx2 &&
+          pxin2 === UInt(0) &&
+          pxin1 === UInt(0) &&
+          io.pxin === UInt(0)){
 
-          secondx2 := x;
-          secondy2 := y;
+            secondx2 := x;
+            secondy2 := y;
+        }
+
+        when(x > rightestx &&
+          pxin2 === UInt(0) &&
+          pxin1 === UInt(0) &&
+          io.pxin === UInt(0)){
+
+            rightestx := x;
+            rightesty := y;
+        }
+      }.elsewhen(firstRight){
+        when(x < secondx1 &&
+          pxin2 === UInt(1) &&
+          pxin1 === UInt(1) &&
+          io.pxin === UInt(1)){
+            secondx1 := x;
+            secondy1 := y;
+        }
+
+        when(x < leftestx &&
+          pxin2 === UInt(1) &&
+          pxin1 === UInt(1) &&
+          io.pxin === UInt(1)){
+            leftestx := x;
+            leftesty := y;
+        }
       }
-
-      when(x > rightestx &&
-        pxin2 === UInt(0) &&
-        pxin1 === UInt(0) &&
-        io.pxin === UInt(0)){
-
-          rightestx := x;
-          rightesty := y;
-      }*/
-
-      when(x < secondx1 &&
-        pxin2 === UInt(1) &&
-        pxin1 === UInt(1) &&
-        io.pxin === UInt(1)){
-          secondx1 := x;
-          secondy1 := y;
-      }
-
-      when(x < leftestx &&
-        pxin2 === UInt(1) &&
-        pxin1 === UInt(1) &&
-        io.pxin === UInt(1)){
-          leftestx := x;
-          leftesty := y;
-      }
-
     }
-
   }
 
   when(x === 639.U){
-    when(leftestx === firstx && leftesty > ( firsty + 5.U)){
-      x0temp := firstx;
-      y0temp := firsty;
-      x1temp := secondx2;
-      y1temp := secondy2;
 
-      done := Bool(true);
+    when(firstLeft){
 
-    }
+      when(rightesty === secondy2 && firsty === secondy2 && secondx2 > (firstx + 40.U)){
+        x0temp := firstx - 3.U;
+        y0temp := firsty;
+        x1temp := secondx2 - 3.U;
+        y1temp := firsty;
 
+        done := Bool(true);
+      }
 
-    when(rightestx < secondx2 && rightesty > (secondy2 + 5.U)){
-      x0temp := firstx;
-      y0temp := firsty;
-      x1temp := secondx2;
-      y1temp := secondy2;
+      when(rightestx < secondx2 && rightesty > (secondy2 + 5.U)){
+        x0temp := firstx - 3.U;
+        y0temp := firsty;
+        x1temp := secondx2 - 3.U;
+        y1temp := secondy2;
 
-      done := Bool(true);
-    }
+        done := Bool(true);
+      }
 
-    when(leftestx > secondx1 && leftesty > (secondy1 + 5.U)){
-      x0temp := secondx1 - 3.U;
-      y0temp := secondy1;
-      x1temp := firstx - 3.U;
-      y1temp := firsty;
+    }.elsewhen(firstRight){
 
-      done := Bool(true);
+      when(leftestx > secondx1 && leftesty > (secondy1 + 5.U)){
+        x0temp := secondx1 - 3.U;
+        y0temp := secondy1;
+        x1temp := firstx - 3.U;
+        y1temp := firsty;
+
+        done := Bool(true);
+      }
     }
   }
-
 }
 
 class CornersModuleTests(c: CornersModule) extends Tester(c) {
-  var x,y, x0,y0,x1,y1 : Int = 0;
+  var i, x0,y0,x1,y1 : Int = 0;
   var done : Boolean = false;
 
-  val imagename : String = "foto3.jpg";
+  /*val imagename : String = "foto2.gray";
+  x0 = 193;
+  y0 = 91;
+  x1 = 501;
+  y1 = 89;*/
+
+  /*val imagename : String = "foto3.gray";
   x0 = 176;
   y0 = 86;
   x1 = 504;
-  y1 = 60;
+  y1 = 60;*/
 
-  /*val imagename : String = "foto6.jpg";
+  //White pixel in the middle of nothing makes this give wrong output
+  /*val imagename : String = "foto4.gray";
+  x0 = 191;
+  y0 = 91;
+  x1 = 496;
+  y1 = 87;*/
+
+  /*val imagename : String = "foto5.gray";
+  x0 = 154;
+  y0 = 86;
+  x1 = 472;
+  y1 = 86;*/
+
+  /*val imagename : String = "foto6.gray";
   x0 = 155;
   y0 = 70;
   x1 = 477;
   y1 = 99;*/
 
-  /*val imagename : String = "foto2.jpg";
-  x0 = 193;
-  y0 = 92;
-  x1 = 501;
-  y1 = 89;*/
+  val imagename : String = "foto7.gray";
+  x0 = 106;
+  y0 = 103;
+  x1 = 443;
+  y1 = 29;
+
 
   poke(c.io.enable,true);
 
-  val photo = ImageIO.read(new File("../../../test_images/" + imagename));
+  val byteArray = Files.readAllBytes(Paths.get("../../../test_images/" + imagename))
 
   while(!done){
-    if((new Color(photo.getRGB(x, y))).getGreen()  >= 128){
-      poke(c.io.pxin, 1);
-    }else{
-      poke(c.io.pxin, 0);
-    }
 
+    poke(c.io.pxin, if ((byteArray(i) & 0xff) < 32) 0 else 1)
+    i = i + 1;
     step(1)
-
-    if(x == 639){
-      y = y + 1;
-      x = 0;
-    }else{
-      x = x + 1;
-    }
 
     if(peek(c.io.done) == 1) {
       done = true;
     }
 
   }
+
+  step(100)
 
   expect(c.io.x0, x0)
   expect(c.io.y0, y0)
