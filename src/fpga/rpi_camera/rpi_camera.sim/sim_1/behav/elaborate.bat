9132a44ecddf6bbd20f46a3c0de93553c95d47e5
Edgar Vedvk <edgar.vedvik@gmail.com>
2016-11-20 17:00:47 +0100

    removed old camera directory

--- src/fpga/rpi_camera/rpi_camera.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.3\\bin
-call %xv_path%/xelab  -wto efa6c0298d674da0895771d923a8dd87 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot camera_tb_behav xil_defaultlib.camera_tb -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- src/fpga/rpi_camera/rpi_camera.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.3\\bin
-call %xv_path%/xsim camera_tb_behav -key {Behavioral:sim_1:Functional:camera_tb} -tclbatch camera_tb.tcl -view C:/Users/Edgar/dmpro16/src/fpga/rpi_camera/camera_tb_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
